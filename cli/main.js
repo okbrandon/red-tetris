8f5f42be7ae6f8bc25b5a5b3d68f89acd375c89b
okbrandon <okbrandonlooksdumb@gmail.com>
2025-05-05 16:19:22 +0200

    cli: Added game over support

--- cli/main.js
+++ cli/main.js
@@ -66,8 +66,6 @@ const spectersBox = blessed.box({
 });
 
 screen.append(box);
-screen.append(nextBox);
-screen.append(spectersBox);
 screen.render();
 
 function createRandomString(length) {
@@ -126,30 +124,65 @@ socket.on("room-joined", (data) => {
   screen.render();
 });
 
+socket.on("room-broadcast", (data) => {
+  const parsedData = JSON.parse(data);
+
+  box.setLabel(`${parsedData.room} - ${parsedData.owner.username}`);
+  box.setContent(`You are player (${parsedData.you.username}) in room ${parsedData.room}.`);
+
+  const clientsText = parsedData.clients
+    .map(client => `> ${client.username}` + (client.id === parsedData.owner.id ? ' (owner)' : ''))
+    .join('\n');
+  nextBox.setContent(clientsText);
+  nextBox.setLabel('Connected Players')
+
+  screen.append(nextBox);
+  screen.render();
+})
+
 socket.on("game-started", (data) => {
   const parsedData = JSON.parse(data);
 
   box.setLabel(`${parsedData.room} - Game started`);
+  nextBox.setLabel('Next Pieces');
+
+  screen.append(spectersBox);
   screen.render();
 });
 
 socket.on("game-state", (data) => {
-  const grid = data.grid;
-  const board = formatBoard(grid);
   const roomLabel = `${data.room.id}@${data.room.owner.username} - ${data.room.status}`;
 
   box.setLabel(`${roomLabel}`);
-  box.setContent(`-- Board --\n${board}`);
 
-  const nextPiecesText = data.nextPieces
-    .map((piece, i) => `Piece #${i + 1}:\n${piece.shape.map(row => row.join(' ')).join('\n')}`)
-    .join('\n\n');
-  nextBox.setContent(nextPiecesText);
+  if (!data.you.hasLost) {
+    const grid = data.grid;
+    const board = formatBoard(grid);
+
+    box.setContent(`-- Board --\n${board}`);
+
+    const nextPiecesText = data.nextPieces
+      .map((piece, i) => `Piece #${i + 1}:\n${piece.shape.map(row => row.join(' ')).join('\n')}`)
+      .join('\n\n');
+    nextBox.setContent(nextPiecesText);
+  }
+  else {
+    box.setContent(`You're done playing for now!\nPlease wait until the game is done...`);
+  }
 
   const spectersText = data.clients
     .map(client => `> ${client.username}\n${(client.specter ? formatBoard(client.specter) : 'Loading...')}`)
     .join('\n\n');
   spectersBox.setContent(spectersText);
+
+  screen.render();
+});
+
+socket.on("game-over", (data) => {
+  box.setLabel(`${data.room.id} - Game Over`);
+  box.setContent(`Message: ${data.message}\nPlease wait until the game is done...`);
+
+  screen.remove(nextBox);
 
   screen.render();
 });
