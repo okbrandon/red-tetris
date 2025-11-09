25190c43c7fbf9258604328b5565e16bd3a2199c
Hanmin <hanmin.01.park@gmail.com>
2025-11-09 17:47:55 +0100

    refactor(animated-background): move shape logic to helpers

--- frontend/src/components/AnimatedBackground/AnimatedBackground.jsx
+++ frontend/src/components/AnimatedBackground/AnimatedBackground.jsx
@@ -5,54 +5,12 @@ import {
   Piece,
   Tile,
 } from './AnimatedBackground.styles.js';
-
-const SHAPES = {
-  I: [
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [3, 0],
-  ],
-  L: [
-    [0, 0],
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ],
-  J: [
-    [1, 0],
-    [1, 1],
-    [0, 1],
-    [0, 2],
-  ],
-  Z: [
-    [0, 0],
-    [1, 0],
-    [1, 1],
-    [2, 1],
-  ],
-  S: [
-    [1, 0],
-    [2, 0],
-    [0, 1],
-    [1, 1],
-  ],
-  T: [
-    [1, 0],
-    [0, 1],
-    [1, 1],
-    [2, 1],
-  ],
-  O: [
-    [0, 0],
-    [1, 0],
-    [0, 1],
-    [1, 1],
-  ],
-};
-
-const TILE_SIZE = 14;
-const DEFAULT_SHAPE = 'O';
+import {
+  getShapeCoordinates,
+  computeDimensions,
+  createPieceStyle,
+  createTileStyle,
+} from './helpers.js';
 
 const PIECE_DEFINITIONS = [
   {
@@ -207,41 +165,6 @@ const PIECE_DEFINITIONS = [
   },
 ];
 
-const getShapeCoordinates = (shape) => SHAPES[shape] ?? SHAPES[DEFAULT_SHAPE];
-
-const computeDimensions = (coordinates) => {
-  const [maxX, maxY] = coordinates.reduce(
-    ([largestX, largestY], [x, y]) => [
-      Math.max(largestX, x),
-      Math.max(largestY, y),
-    ],
-    [0, 0]
-  );
-
-  return {
-    width: (maxX + 1) * TILE_SIZE,
-    height: (maxY + 1) * TILE_SIZE,
-  };
-};
-
-const createPieceStyle = (vars, { width, height }) => ({
-  '--x': vars.x,
-  '--xEnd': vars.xEnd,
-  '--dur': vars.dur,
-  '--delay': vars.delay,
-  '--rStart': vars.rStart,
-  '--rEnd': vars.rEnd,
-  '--scale': vars.scale,
-  '--alpha': vars.alpha,
-  width: `${width}px`,
-  height: `${height}px`,
-});
-
-const createTileStyle = ([x, y]) => ({
-  left: `${x * TILE_SIZE}px`,
-  top: `${y * TILE_SIZE}px`,
-});
-
 const PIECES = PIECE_DEFINITIONS.map((piece) => {
   const coordinates = getShapeCoordinates(piece.shape);
   const dimensions = computeDimensions(coordinates);
@@ -253,14 +176,6 @@ const PIECES = PIECE_DEFINITIONS.map((piece) => {
     tileOffsets: coordinates.map(createTileStyle),
   };
 });
-
-// eslint-disable-next-line react-refresh/only-export-components
-export {
-  getShapeCoordinates,
-  computeDimensions,
-  createPieceStyle,
-  createTileStyle,
-};
 
 const AnimatedBackground = () => (
   <Background>
